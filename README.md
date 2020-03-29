5c149071b7b5026d122af56c6b4db49d977fd0d4
dervincy <dervincy@gmail.com>
2020-03-30 01:36:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-nh6gfd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-nh6gfd)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function main(){
+  console.log('Hello from Main')
+  console.debug('Hello from debug')
+  console.warn('Hello from warn')
+  console.error('Hello from error')
+  console.info('Hello from info')
+}
+
+main();
+
